90781e450b6d04428c347b478a221bc5170a74f3
yuliagushcha <Yulia_Gushcha@mail.ru>
2019-10-06 17:24:19 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let multiplication = [];
+  let firstNumber = [];
+  let secondNumber = [];
+  
+    firstNumber.push(first.substr(0, first.length));
+    console.log(firstNumber);
+  
+    secondNumber.push(second.substr(0, second.length));
+    console.log(secondNumber);
+
+    
+    multiplication = ""+BigInt(firstNumber*secondNumber)+"";
+    console.log(multiplication);
+  return multiplication;
 }
