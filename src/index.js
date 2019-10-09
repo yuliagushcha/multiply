0315c751fc7ae54f190fb9135c08a09d2bb257ad
yuliagushcha <Yulia_Gushcha@mail.ru>
2019-10-10 01:02:40 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 module.exports = function multiply(first, second) {
-  let multiplication = [];
-  let firstNumber = [];
-  let secondNumber = [];
-  
-    firstNumber.push(first.substr(0, first.length));
-    console.log(firstNumber);
-  
-    secondNumber.push(second.substr(0, second.length));
-    console.log(secondNumber);
+  let firstreversed = first.split('').reverse();
+  let secondreversed = second.split('').reverse();
 
-    
-    multiplication = ""+BigInt(firstNumber*secondNumber)+"";
-    console.log(multiplication);
-  return multiplication;
+  let arr = [];
+
+  for (let i = 0; i < firstreversed.length; i++) {
+    for (let j = 0; j < secondreversed.length; j++) {
+      let multiplication = firstreversed[i] * secondreversed[j];
+      arr[i + j] = (arr[i + j]) ? arr[i + j] + multiplication : multiplication;
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let newnumber = arr[i] % 10;
+    let moving = Math.floor(arr[i] / 10);
+    arr[i] = newnumber;
+
+    if (arr[i + 1])
+      arr[i + 1] += moving;
+    else if (moving != 0)
+      arr[i + 1] = moving;
+  }
+
+
+  return arr.reverse().join('');
 }
